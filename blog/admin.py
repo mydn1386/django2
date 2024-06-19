96f5dae7dfe77996fe92e7ed519987c03603c0a4
mydn1386 <danshnia86@gmail.com>
2024-06-19 23:14:49 +0330

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,41 @@
+# actions = [delete_selected]
+# @admin.action(description='حذف انتخاب شده‌ها')
+# def delete_selected(modeladmin, request, queryset):
+#     queryset.delete()
+
+# actions = [delete_selected]
+
+from django.contrib import admin
+from .models import Post, Account
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'status', 'published')
+    list_filter = ('status', 'created', 'updated', 'author')
+    search_fields = ('title', 'caption')
+    raw_id_fields = ('author',)
+    date_hierarchy = 'published'
+    ordering = ('status', '-published')
+    list_editable = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    list_display_links = ('slug', 'title',)
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('user_email', 'user_first_name', 'user_last_name')
+    search_fields = ('user__email', 'user__first_name', 'user__last_name')
+
+    def user_email(self, obj):
+        return obj.user.email
+    user_email.short_description = 'Email'
+
+    def user_first_name(self, obj):
+        return obj.user.first_name
+    user_first_name.short_description = 'First Name'
+
+    def user_last_name(self, obj):
+        return obj.user.last_name
+    user_last_name.short_description = 'Last Name'
+
---
+++ blog/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import Account
+
+
+# class AccountForm(forms.ModelForm):
+#     class Meta:
+#         model = Account
+#         fields = ('phone', )
+
+
+class AccountForm(forms.Form):
+    GENDER_CHOICES = (
+        ('آقا', "آقا"),
+        ('خانم', 'خانم'),
+    )
+    gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.RadioSelect, label='جنسیت')
+    address = forms.CharField(max_length=200, widget=forms.Textarea, label='آدرس', required=True)
+    first_name = forms.CharField(max_length=15, label='نام', required=True)
+    last_name = forms.CharField(max_length=15, label='نام خانوادگی', required=True)
+    birth = forms.CharField(max_length=10, label='تاریخ تولد', required=True)
+
+    # def clean(self):
+    #
+
+#
+# # from django import forms
+# # from .models import Account
+#
+# class AccountForm(forms.ModelForm):
+#     class Meta:
+#         model = Account
+#         fields = ('first_name', 'last_name', 'gender', 'address', 'birth')
