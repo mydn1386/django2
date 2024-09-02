27c42568f54e3b04206e883e51d88b44c32c2e67
mydn1386 <danshnia86@gmail.com>
2024-09-02 12:31:12 +0330

    .

--- djangoProject5/settings.py
+++ djangoProject5/settings.py
@@ -86,24 +86,24 @@ WSGI_APPLICATION = 'djangoProject5.wsgi.application'
 # }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'MusicPlayer',
-#         'USER': 'postgres',
-#         'PORT': '5432',
-#         "HOST": "127.0.0.1",
-#         'PASSWORD': '1745233024d',
-#
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'MusicPlayer',
+        'USER': 'postgres',
+        'PORT': '5432',
+        "HOST": "127.0.0.1",
+        'PASSWORD': '1745233024d',
+
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
